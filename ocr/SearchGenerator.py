005445b1c42545a137cc65b3518749d9007b15da
Gigantor2131 <44032190+Gigantor2131@users.noreply.github.com>
2026-03-23 14:21:04 -0600

    log exact frame ints in addition to pretty times in order to track exact looping behavior

--- ocr/SearchGenerator.py
+++ ocr/SearchGenerator.py
@@ -24,9 +24,9 @@ class SearchGenerator:
     # Pass negative values to go in reverse
     def seconds_based_skip(self, skip_size: VidPos):
         if skip_size > VidPos(frame=0):
-            log.info(f"Searching {self.start.pretty_time()} --> {self.stop.pretty_time()}")
+            log.info(f"Searching {self.start.frame} --> {self.stop.frame} {self.start.pretty_time()} --> {self.stop.pretty_time()}")
         else:
-            log.info(f"Searching {self.stop.pretty_time()} <-- {self.start.pretty_time()}")
+            log.info(f"Searching {self.stop.frame} <-- {self.start.frame} {self.stop.pretty_time()} <-- {self.start.pretty_time()}")
         def skipper(msg, frame):
             self.pos += skip_size
         return self.communicator(skipper)
