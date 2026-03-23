a7297b895ed2192c884230d27955cca9a6f5b3e6
Gigantor2131 <44032190+Gigantor2131@users.noreply.github.com>
2026-03-22 20:10:14 -0600

    use fastapi not __main__

---
+++ README.md
@@ -0,0 +1,6 @@
+# VEX OCR
+
+use the below to run the server, then post OCR jobs to it. 
+```bash 
+fastapi dev
+```
--- main.py
+++ main.py
@@ -28,9 +28,3 @@ app = FastAPI(lifespan=lifespan)
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=False)
